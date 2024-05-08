851cb792daca23652e2d4cd2c34e8e5454e05868
l20023 <leo20kustermann@gmail.com>
2024-05-08 11:59:41 +0200

    Hinzufügen von Website via Flask. Name:WahlOmat. Kann noch geändert werden

---
+++ AAChatPolite/WahlOmat/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, session
+from openai import OpenAI
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = b'sk-proj-poVcYbfC5Cy3NgbvNhUIT3BlbkFJqtLU4GwHwGKkKq7iFANg'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+#openai.api_key = 'sk-proj-poVcYbfC5Cy3NgbvNhUIT3BlbkFJqtLU4GwHwGKkKq7iFANg'
+client = OpenAI()
+
+def get_data(query):
+  completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant for politics"},
+    {"role": "user", "content": session['query']}
+  ]
+  )
+  response = completion.choices[0].message.content
+  return response
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        query = request.form['q']
+        # Setze die Abfrage in der Session
+        session['query'] += "Question: " + query + "\n"
+        session.modified = True
+        # Abrufen der Daten
+        response = get_data(query)
+        session['query'] += "You answered: " + response + "\n"
+        session.modified = True
+        
+        return render_template("index.html", question=query, response=response)
+    # Wenn GET-Anfrage, zeige einfach das Formular an
+    return render_template("index.html", question='query', response="response")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
