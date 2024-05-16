49abb433020eefff61fe9a3e5c78293de0e79b63
Nikolaus Brissa <nbrissa@student.ethz.ch>
2024-05-16 21:05:44 +0200

    start to translate

--- Frontend/elect-o-mate/src/App.js
+++ Frontend/elect-o-mate/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import './index.css';
 import Footer from './components/Footer';
 import Chat from './components/Chat';
@@ -6,10 +7,11 @@ import Top from './components/Top';
 import OpenSource from './components/OpenSource';
 import Spline from './components/Spline';
 import Questionnaire from './components/Questionnaire';
+import './i18n';
 
 function App() {
   const targetRef = useRef(null);
-  // in userLanguage ist die Sprache/das Land gespeichert
+  const { t, i18n } = useTranslation();
   const [userLanguage, setUserLanguage] = useState('en'); // Default to English
 
   const handleButtonClick = () => {
@@ -23,7 +25,7 @@ function App() {
     try {
       const response = await fetch('https://ipinfo.io/json');
       const data = await response.json();
-      return data.country; 
+      return data.country;
     } catch (error) {
       console.error('Fehler beim Abrufen des Landes aus der IP-Adresse:', error);
       return null;
@@ -35,19 +37,21 @@ function App() {
       try {
         const userCountry = await getUserCountryFromIP();
         setUserLanguage(userCountry);
+        i18n.changeLanguage(userCountry);
       } catch (error) {
         console.error('Error fetching user location:', error);
         setUserLanguage('en');
+        i18n.changeLanguage('en');
       }
     };
 
     fetchUserLocationAndSetLanguage();
-  }, []);
+  }, [i18n]);
 
   return (
     <div className="flex flex-col relative overflow-hidden">
       <div className="absolute top-[-500px] right-[-500px] w-[1600px] h-[1600px] bg-gradient-to-r from-orange-200 to-orange-50  rounded-full z-0"/>
-      
+
       <div className="flex flex-col items-center pt-20 pb-10 w-full  z-10">
         <div className='w-1/2 z-10 pt-25'>
           <Top onButtonClick={handleButtonClick} />
@@ -69,15 +73,14 @@ function App() {
 
       <div className="relative mt-10 mb-20">
         <div className="absolute top-0 left-0 w-full bg-gradient-to-r from-green-900  to-blue-900 transform skew-y-3"  style={{ height: '110%' }}/>
-          <div className="container mx-auto px-4 relative z-10 w-1/2">
-            <OpenSource />
+        <div className="container mx-auto px-4 relative z-10 w-1/2">
+          <OpenSource />
         </div>
       </div>
 
       <div className="mt-5 w-full bg-gradient-to-r from-orange-50 via-orange-100 to-orange-300">
         <Footer />
       </div>
-
     </div>
   );
 }
--- Frontend/elect-o-mate/src/components/Top.js
+++ Frontend/elect-o-mate/src/components/Top.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Top = ({ onButtonClick }) => {
+  const { t } = useTranslation();
   return (
     <header className="text-black pt-40 m-10 text-center">
       <h1 className="text-8xl font-extrabold">Elect-O-Mate</h1>
       <p className="mt-4">
-      Providing voters with clear and concise information to enable informed decisions during elections.
+      {t('subheading')}
       </p>
       <button 
       className="m-8 py-4 px-9 pt-2 bg-gradient-to-r from-green-500  to-blue-500 text-white rounded-full font-semibold transition duration-300 ease-in-out transform hover:bg-gradient-to-r hover:from-pink-500 hover:to-indigo-500 hover:scale-110 text-xl"
         onClick={onButtonClick}
       >
-        Start now
+        {t('start_button')}
       </button>
     </header>
   );
