7fcafbf3ed7d701a94935127168fa78b87f85f94
l20023 <leo20kustermann@gmail.com>
2024-05-19 12:57:59 +0200

    button stays bottom right in chatwindow

--- Frontend/elect-o-mate/src/components/ChatWindow.js
+++ Frontend/elect-o-mate/src/components/ChatWindow.js
@@ -36,68 +36,65 @@ const ChatWindow = ({ messages, scrollToQuestionnaire }) => {
 
   return (
     <div
-      className='relative overflow-y-auto border-none shadow-xl border-gray-300 flex flex-col justify-between bg-white h-[600px] md:[700px]'
+      className='relative flex flex-col overflow-y-auto border-none shadow-xl border-gray-300 bg-white h-[600px] md:h-[700px]'
       style={{
-        overflowY: 'auto',
         WebkitOverflowScrolling: 'touch',
         scrollbarWidth: 'thin',
         borderRadius: '10px',
       }}
       ref={chatWindowRef}
     >
+      <div className='p-4 flex-grow overflow-y-auto'>
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`mb-2 ${message.isUser ? 'text-right' : 'text-left'}`}
+          >
+            {message.isUser ? (
+              <div className='flex flex-col items-end'>
+                <p className='font-bold text-gray-700'>{t('chat_YOU')}</p>
+                <p
+                  className='text-gray-600 p-2 rounded-md text-left'
+                  style={{ wordBreak: 'break-word', background: '#A1BBB8' }}
+                >
+                  {message.text}
+                </p>
+              </div>
+            ) : (
+              <div className='flex flex-col items-start'>
+                <p
+                  className={`font-bold ${
+                    message.isError ? 'text-red-700' : 'text-blue-700'
+                  }`}
+                >
+                  <span className='inline-block align-middle rounded-full overflow-hidden m-2'>
+                    <img
+                      src={chatbot}
+                      alt='Bot Profile'
+                      style={{ height: '1.9em', width: '1.9em' }}
+                    />
+                  </span>
+                  {message.isError ? 'Error' : 'Elect-O-Mate'}
+                </p>
+                <p
+                  className='text-gray-600 p-2 rounded-md text-left break-words'
+                  style={{ background: '#A1BBB8' }}
+                >
+                  {convertTextToLinks(message.text)}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
       <button
-        className='absolute py-5 px-5 text-black rounded-full font-thin transition duration-300 ease-in-out transform hover:scale-110 text-xl right-0 bottom-0 z-10 bg-black bg-opacity-20 backdrop-blur-lg'
+        className='absolute py-5 px-5 text-black rounded-full font-thin transition duration-300 ease-in-out transform hover:scale-110 text-xl right-5 bottom-5 z-10 bg-black bg-opacity-20 backdrop-blur-lg'
         onClick={scrollToQuestionnaire}
       >
         <ArrowUpwardIcon />
       </button>
-
-      <div className='p-4'>
-        <div className='mb-2 flex-grow'>
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`mb-2 ${message.isUser ? 'text-right' : 'text-left'}`}
-            >
-              {message.isUser ? (
-                <div className='flex flex-col items-end overflow-y-auto'>
-                  <p className='font-bold text-gray-700'>{t('chat_YOU')}</p>
-                  <p
-                    className='text-gray-600 p-2 rounded-md text-left'
-                    style={{ wordBreak: 'break-word', background: '#A1BBB8' }}
-                  >
-                    {message.text}
-                  </p>
-                </div>
-              ) : (
-                <div className='flex flex-col items-start'>
-                  <p
-                    className={`font-bold ${
-                      message.isError ? 'text-red-700' : 'text-blue-700'
-                    }`}
-                  >
-                    <span className='inline-block align-middle rounded-full overflow-hidden m-2'>
-                      <img
-                        src={chatbot}
-                        alt='Bot Profile'
-                        style={{ height: '1.9em', width: '1.9em' }}
-                      />
-                    </span>
-                    {message.isError ? 'Error' : 'Elect-O-Mate'}
-                  </p>
-                  <p
-                    className='text-gray-600 p-2 rounded-md text-left break-words'
-                    style={{ background: '#A1BBB8' }}
-                  >
-                    {convertTextToLinks(message.text)}
-                  </p>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
     </div>
   );
 };
+
 export default ChatWindow;
