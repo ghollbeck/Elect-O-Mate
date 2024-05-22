e6f7a6898208acd5636188d43ec384c39b700011
Nikolaus Brissa <nbrissa@student.ethz.ch>
2024-05-22 16:03:48 +0200

    merge eval to main

--- Backend/main.py
+++ Backend/main.py
@@ -12,7 +12,11 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.document_loaders import WebBaseLoader
 
+
 from langchain_community.vectorstores import FAISS
+from langchain_chroma import Chroma
+
+
 from langchain_community.vectorstores import Chroma
 
 from langchain_text_splitters import CharacterTextSplitter
