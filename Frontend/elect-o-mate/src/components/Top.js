7495331a5f6f2b02b3967caf06ee663478b4e1e1
Nikolaus Brissa <nbrissa@student.ethz.ch>
2024-05-29 20:30:00 +0200

    fix button in chrome

--- Frontend/elect-o-mate/src/components/Top.js
+++ Frontend/elect-o-mate/src/components/Top.js
@@ -12,7 +12,7 @@ const Top = ({
   const { t } = useTranslation();
   return (
     <div className='my-10 p-5'>
-      <header className='text-center  bg-gradient-to-r from-violet-100 to-pink-300 inline-block text-transparent bg-clip-text'>
+      <header className='text-center  bg-gradient-to-r from-violet-100 to-pink-300 inline-block text-transparent bg-clip-text '>
         <h1 className='text-6xl md:text-9xl font-extrabold'>Elect-O-Mate</h1>
         {/* <p className='mt-4'>{t('subheading')}</p> */}
         <div className='w-full flex-col justify-center mt-4'>
@@ -32,7 +32,7 @@ const Top = ({
             {t('start_now')}
           </div>
           <button
-            className=' py-4 px-9 rounded-full font-semibold  transition duration-300 ease-in-out transform hover:scale-110 text-xl shadow-lg border'
+            className=' py-4 px-9 rounded-full font-semibold  transition duration-300 ease-in-out transform hover:scale-110 text-xl shadow-lg border text-pink-200'
             onClick={onButtonClick}
           >
             {t('start_button')}
