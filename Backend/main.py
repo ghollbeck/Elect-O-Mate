452834abb07a0b6b8a0bfa1742938dce7747bde4
Lorin Urbantat <lurbantat@ethz.ch>
2024-08-18 17:13:42 +0200

    fixed updated chroma db

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 #from langchain_community.vectorstores import Chroma
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain.storage import LocalFileStore
